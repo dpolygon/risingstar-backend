f7f418b5040791477e717c5186a938652b6f52c3
dpolygon <dannyg@lonestardev.net>
2024-03-08 03:42:10 -0600

    partially functioning backend when on production

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, send_from_directory, abort, request
 from flask_mail import Mail, Message
 from dotenv import load_dotenv
 from flask_cors import CORS, cross_origin
+
 import requests
 import os
 import functions_framework
@@ -148,3 +149,6 @@ def get_pdf(file_name):
         return send_from_directory(app.config["CLIENT_PDFS"], path=file_name, as_attachment=True)
     except FileNotFoundError:
         abort(404)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
